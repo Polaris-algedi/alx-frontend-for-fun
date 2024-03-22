1944757929eedf6bce88956c3df089853faa892f
Polaris-algedi <karchalmohammed@gmail.com>
2024-03-22 19:55:57 +0000

    Refactor markdown to HTML conversion and add functions for reading and writing files

--- markdown2html.py
+++ markdown2html.py
@@ -13,18 +13,51 @@ If the source file doesn't exist, it will also print an error
 message and exit with code 1.
 Otherwise, it will exit with code 0 indicating successful execution.
 """
-import sys, os
 
-if len(sys.argv) < 3:
-    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-    sys.exit(1)
-elif not os.path.exists(sys.argv[1]):
-    print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-    sys.exit(1)
-else:
-    sys.exit(0)
+import sys
+import os
+
+# Convert markdown to html
+
+
+def markdown_to_html(markdown_lines):
+    html_lines = []
+    html_unordered_list = []
+    i = 0
+    num_lines = len(markdown_lines)
+    while i < num_lines:
+        if markdown_lines[i].startswith('-'):
+            while i < num_lines and markdown_lines[i].startswith('-'):
+                html_unordered_list.append(markdown_lines[i][2:])
+                i += 1
+            html_lines.extend(to_html_unordered_list(html_unordered_list))
+            html_unordered_list = []
+        if i < num_lines and markdown_lines[i].startswith('#'):
+            html_lines.append(to_html_heading(markdown_lines[i]))
+        elif i < num_lines:
+            html_lines.append(markdown_lines[i])
+        i += 1
+    return html_lines
+
+# Read file function
+
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        markdown_lines = file.readlines()
+    return markdown_lines
+
+# Write file function
+
+
+def write_file(file_path, html_lines):
+    with open(file_path, 'w') as file:
+        for line in html_lines:
+            file.write(line)
 
 # I will use this function later
+
+
 def to_html_heading(markdown_line):
     heading_dict = {
         'level_1': ['<h1>', '</h1>'],
@@ -46,8 +79,32 @@ def to_html_heading(markdown_line):
 
     html_heading = "{}{}{}".format(
         correct_html_tags[0],
-        heading_text,
+        heading_text.strip(),
         correct_html_tags[1]
-        )
+    )
 
     return html_heading
+
+# To HTML Unordered List
+
+
+def to_html_unordered_list(markdown_lines):
+    html_lines = ['<ul>\n']
+    for line in markdown_lines:
+        html_lines.append('\t<li>{}</li>\n'.format(line.strip()))
+
+    html_lines.append('</ul>\n')
+    return html_lines
+
+
+if len(sys.argv) < 3:
+    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+    sys.exit(1)
+elif not os.path.exists(sys.argv[1]):
+    print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+    sys.exit(1)
+else:
+    markdown_lines = read_file(sys.argv[1])
+    html_lines = markdown_to_html(markdown_lines)
+    write_file(sys.argv[2], html_lines)
+    sys.exit(0)
